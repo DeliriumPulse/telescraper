70e26a0115a3aef2564688c756d22ff1204cf33b
DeliriumPulse <zwhitson01@gmail.com>
2025-12-30 06:07:49 -0600

    Verification: Update build specs to match Telescraper branding

--- build_exe.bat
+++ build_exe.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Building Telescraper EXE...
 rmdir /s /q build dist
-py -m PyInstaller TelegramForwarder.spec
+py -m PyInstaller Telescraper.spec
 echo Build complete. The executable is in the 'dist' folder.
 pause
